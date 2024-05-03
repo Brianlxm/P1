6b44eed7e4e225805555ea1218eba0592641e929
Brian Lam <brianjohnlam@gmail.com>
2024-05-02 21:21:39 -0400

    worked on loginUser method

--- P1-backend/src/main/java/com/revature/controllers/UserController.java
+++ P1-backend/src/main/java/com/revature/controllers/UserController.java
@@ -2,10 +2,14 @@ package com.revature.controllers;
 
 import com.revature.models.DTOs.LoginUserDTO;
 import com.revature.models.DTOs.RegisterUserDTO;
+import com.revature.models.User;
 import com.revature.services.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -29,12 +33,22 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody LoginUserDTO userDTO){
-        try{
-            userService.loginUser(userDTO);
-            return ResponseEntity.status(200).body("Welcome " + userDTO.getUsername() + " !");
-        } catch (IllegalArgumentException e){
-            return ResponseEntity.status(400).body(e.getMessage());
+    public ResponseEntity<?> loginUser(@RequestBody LoginUserDTO userDTO, HttpSession session){
+        //get user object from the service
+        Optional<User> optionalUser = userService.loginUser(userDTO);
+
+        if(optionalUser.isEmpty()){
+            return ResponseEntity.status(401).body("Login Failed!");
         }
+
+        //if login succeeds store the userinfo in our session
+        User u = optionalUser.get();
+
+        //store the user info in our session
+        session.setAttribute("userId", u.getUserId());
+        session.setAttribute("username", u.getUsername());
+        session.setAttribute("role", u.getRole());
+
+        
     }
 }
